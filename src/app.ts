6f992396ea50f9ef84bae74a1470abf54fab8db1
Dani <d.tarazoma@geekshubs.com>
2023-01-11 16:34:29 +0100

    feat: add home and register route

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,9 @@ const app = express();
 
 const PORT = process.env.PORT || 3006
 
+app.get('/', (__req: Request, res: Response) => res.send('welcome to my app'))
 app.get('/api/login', (__req: Request, res: Response) => res.send('login'));
+app.get('/api/register', (__req: Request, res: Response) => res.send('register'));
+
 
 app.listen(PORT, () => console.log("Server running on port: " + PORT));
