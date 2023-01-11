53083cd0e6ddf54c6e9c483483ab907b5369d0f9
Dani <d.tarazoma@geekshubs.com>
2023-01-11 16:35:01 +0100

    feat: deploy

--- dist/app.js
+++ dist/app.js
@@ -6,5 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 3006;
+app.get('/', (__req, res) => res.send('welcome to my app'));
 app.get('/api/login', (__req, res) => res.send('login'));
+app.get('/api/register', (__req, res) => res.send('register'));
 app.listen(PORT, () => console.log("Server running on port: " + PORT));
