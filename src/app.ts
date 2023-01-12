da9f3f8a84179ba06a1bc1307ee391e4fc465f4a
Dani <d.tarazoma@geekshubs.com>
2023-01-12 09:17:51 +0100

    feat: bcrypt added, register endpoint modified

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,36 @@
 import express, { Request, Response } from "express";
+import bcrypt from "bcrypt";
 require('dotenv').config();
 
 const app = express();
+app.use(express.json());
 
 const PORT = process.env.PORT || 3006
 
 app.get('/', (__req: Request, res: Response) => res.send('welcome to my app'))
-app.get('/api/login', (__req: Request, res: Response) => res.send('login'));
-app.get('/api/register', (__req: Request, res: Response) => res.send('register'));
+
+//Auth
+app.post('/api/register', (req: Request, res: Response) => {
+    const { email, name, password} =  req.body;
+
+    const saltRounds: number = 10;
+    const encryptedPassword: string = bcrypt.hashSync(password, saltRounds);
+    
+    const newUser = {
+        email,
+        name,
+        password: encryptedPassword
+    }
+    
+    //TODO save new user into DB
+
+    return res.status(200).json({
+        success: true,
+        message: "User registered"
+    })
+});
+
+app.get('/api/login', (__req: Request, res: Response) => {res.send('login')});
 
 
 app.listen(PORT, () => console.log("Server running on port: " + PORT));
