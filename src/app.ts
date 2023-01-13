907a33c5253022b28a9cd68e41af3c68a09e6598
Dani <d.tarazoma@geekshubs.com>
2023-01-13 12:27:40 +0100

    feat: register users

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
-import express, { Request, Response } from "express";
+import express, { ErrorRequestHandler, Request, Response } from "express";
 import bcrypt from "bcrypt";
 require('dotenv').config();
+
+import { prisma } from "./config/db";
 
 const app = express();
 app.use(express.json());
@@ -10,27 +12,45 @@ const PORT = process.env.PORT || 3006
 app.get('/', (__req: Request, res: Response) => res.send('welcome to my app'))
 
 //Auth
-app.post('/api/register', (req: Request, res: Response) => {
-    const { email, name, password} =  req.body;
+app.post('/api/register', async (req: Request, res: Response) => {
+    const { email, name, password } = req.body;
 
-    const saltRounds: number = 10;
-    const encryptedPassword: string = bcrypt.hashSync(password, saltRounds);
-    
-    const newUser = {
-        email,
-        name,
-        password: encryptedPassword
+    try {
+        const saltRounds: number = 10;
+        const encryptedPassword: string = bcrypt.hashSync(password, saltRounds);
+
+        const newUser: any = {
+            email,
+            name,
+            password: encryptedPassword
+        };
+
+        await prisma.user.create({ data: newUser });
+
+        return res.status(200).json({
+            success: true,
+            message: "User registered"
+        });
+    } catch (error: any) {
+        if (error.code === "P2002") {
+            return res.status(400).json({
+                success: false,
+                message: "User can´t be registered",
+                error: "User cant be registered"
+            });
+        }
+
+        return res.status(500).json({
+            success: false,
+            message: "User can´t be registered",
+            error: "User cant be registered"
+        });
+    } finally {
+        prisma.$disconnect;
     }
-    
-    //TODO save new user into DB
-
-    return res.status(200).json({
-        success: true,
-        message: "User registered"
-    })
 });
 
-app.get('/api/login', (__req: Request, res: Response) => {res.send('login')});
+app.get('/api/login', (__req: Request, res: Response) => { res.send('login') });
 
 
 app.listen(PORT, () => console.log("Server running on port: " + PORT));
