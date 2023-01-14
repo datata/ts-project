fee0dd3890c3d973f7bea418f56987af6869b549
datata <dani@danitarazona.dev>
2023-01-14 15:36:02 +0100

    feat: deploy

--- dist/app.js
+++ dist/app.js
@@ -5,11 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 require('dotenv').config();
-const cors_1 = __importDefault(require("cors"));
 const register_controller_1 = __importDefault(require("./auth/infrastructure/controllers/registerController/register.controller"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
-app.use((0, cors_1.default)());
 const PORT = process.env.PORT || 3006;
 app.get('/', (__req, res) => res.send('welcome to my app'));
 app.post('/api/register', register_controller_1.default);
